11157f1a82a54e00fa2700d1bf2125d3fb277240
Alberto Ramon Trejo Ramirez <alberto.trejor@coppel.com>
2024-10-07 13:34:10 -0600

    Initial commit

---
+++ src/main/java/com/demo_microservice/DemoMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.demo_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo_microservice/DemoMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
